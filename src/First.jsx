88702d66849f45ac08fca0b1bed5b0382ccdb8d5
giorg <klitougiorgos@gmail.com>
2024-05-31 19:26:45 +0100

    added initial structure

--- src/First.jsx
+++ src/First.jsx
@@ -1,7 +1,9 @@
+import PFP from "./assets/PFP.jpg"
+
 function First(){
     return(
-        <div className="picture">
-        <img></img>
+        <div className="picture-div">
+        <img className="PFP" src={PFP} alt="Profile Picture"></img>
         <h2>Georgios Kleitou</h2>
         <h3>Software Engineer</h3>
     </div>
